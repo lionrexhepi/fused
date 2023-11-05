9d5fe0eb3867a2d478b625e89bc8a6518621eb4c
Lion Rexhepi <94755628+lionrexhepi@users.noreply.github.com>
2023-11-05 19:08:18 +0000

    extract tokenization functionality

--- parser/src/tokens.rs
+++ parser/src/tokens.rs
@@ -1,68 +1,250 @@
-use std::{collections::VecDeque, io::Read};
+use core::panic;
+use std::collections::VecDeque;
 
-use crate::{
-    file::{SourceFile, SourceFileError},
-    location::SourceLocation,
-};
+use crate::{ file::{ SourceFile, SourceFileError }, location::SourceLocation };
 
 pub struct Ident(String);
+
+type Result<T> = std::result::Result<T, TokenError>;
 
 pub enum Literal {
     Number(String),
     String(String),
 }
 
+impl Literal {
+    pub fn parse_number(file: &mut SourceFile) -> Result<Self> {
+        let (mut number, _) = file.until(|c| !matches!(c, '0'..='9'))?;
+
+        if let crate::file::Result::Ok(Some((c, _))) = file.next_if(|c| c == '.') {
+            number.push(c);
+            number.push_str(&file.until(|c| !matches!(c, '0'..='9'))?.0);
+        }
+
+        Ok(Self::Number(number))
+    }
+
+    pub fn parse_string(file: &mut SourceFile, quote: char) -> Result<Self> {
+        let (mut string, _) = file.until(|c| (c == quote || c == '\\'))?;
+
+        match file.peek()?.0 {
+            append @ ('\\' | '"' | '\'') => {
+                file.next()?;
+                string.push(append);
+            }
+            'n' => {
+                file.next()?;
+                string.push('\n');
+            }
+            'r' => {
+                file.next()?;
+                string.push('\r');
+            }
+            't' => {
+                file.next()?;
+                string.push('\t');
+            }
+            '0' => {
+                file.next()?;
+                string.push('\0');
+            }
+            'x' => {
+                file.next()?;
+                let (hex, _) = file.next_seq(2)?;
+                let char_code = match u8::from_str_radix(&hex, 16) {
+                    Ok(code) => code,
+                    Err(_) => {
+                        return Err(TokenError::InvalidEscape(hex));
+                    }
+                };
+                string.push(char_code as char);
+            }
+            other @ _ => {
+                return Err(TokenError::InvalidEscape(other.to_string()));
+            }
+        }
+
+        Ok(Self::String(string))
+    }
+}
+
 pub enum Punct {
-    Plus,         //+
-    Minus,        //-
-    Star,         //*
-    Slash,        // /
-    Percent,      // %
-    Caret,        // ^
-    Ampersand,    // &
-    Pipe,         // |
-    DoublePipe,   // ||
-    PlusEq,       // +=
-    MinusEq,      // -=
-    StarEq,       // *=
-    SlashEq,      // /=
-    PercentEq,    // %=
-    CaretEq,      // ^=
-    AmpersandEq,  // &=
-    PipeEq,       // |=
-    Tilde,        // ~
-    Question,     // ?
-    Exclamation,  // !
-    Dot,          // .
-    Comma,        // ,
-    Colon,        // :
-    SemiColon,    // ;
-    Eq,           // =
-    DoubleEq,     // ==
-    NotEq,        // !=
-    Lt,           // <
-    LeftShift,    // <<
-    LeftShiftEq,  // <<=
-    LtEq,         // <=
-    Gt,           // >
-    RightShift,   // >>
+    Plus, //+
+    Minus, //-
+    Star, //*
+    Slash, // /
+    Percent, // %
+    Caret, // ^
+    Ampersand, // &
+    Pipe, // |
+    DoublePipe, // ||
+    PlusEq, // +=
+    MinusEq, // -=
+    StarEq, // *=
+    SlashEq, // /=
+    PercentEq, // %=
+    CaretEq, // ^=
+    AmpersandEq, // &=
+    PipeEq, // |=
+    Tilde, // ~
+    Question, // ?
+    Exclamation, // !
+    Dot, // .
+    Comma, // ,
+    Colon, // :
+    SemiColon, // ;
+    Eq, // =
+    DoubleEq, // ==
+    NotEq, // !=
+    Lt, // <
+    LeftShift, // <<
+    LeftShiftEq, // <<=
+    LtEq, // <=
+    Gt, // >
+    RightShift, // >>
     RightShiftEq, // >>=
-    GtEq,         // >=
-    LBrace,       // {
-    RBrace,       // }
-    LBracket,     // [
-    RBracket,     // ]
-    LParen,       // (
-    RParen,       // )
-    Arrow,        // ->
-    FatArrow,     // =>
-    Hash,         // #
-    Dollar,       // $
-    At,           // @
-    Underscore,   // _
-    Backslash,    // \
+    GtEq, // >=
+    LBrace, // {
+    RBrace, // }
+    LBracket, // [
+    RBracket, // ]
+    LParen, // (
+    RParen, // )
+    Arrow, // ->
+    FatArrow, // =>
+    Hash, // #
+    Dollar, // $
+    At, // @
+    Underscore, // _
+    Backslash, // \
     Newline,
     DoubleAmpersand, // \n
+}
+
+impl Punct {
+    pub const fn is_punct_char(c: char) -> bool {
+        matches!(
+            c,
+            '+' |
+                '-' |
+                '*' |
+                '/' |
+                '%' |
+                '^' |
+                '&' |
+                '|' |
+                '~' |
+                '?' |
+                '!' |
+                '.' |
+                ',' |
+                ':' |
+                ';' |
+                '=' |
+                '<' |
+                '>' |
+                '{' |
+                '}' |
+                '[' |
+                ']' |
+                '(' |
+                ')' |
+                '#' |
+                '$' |
+                '@' |
+                '_' |
+                '\\'
+        )
+    }
+
+    pub fn parse(file: &mut SourceFile) -> Result<Self> {
+        Ok(match file.next()?.0 {
+            '+' => {
+                if file.next_is('=')? { Self::PlusEq } else { Self::Plus }
+            }
+            '-' => {
+                if file.next_is('>')? {
+                    if file.next_is('>')? { Self::FatArrow } else { Self::Arrow }
+                } else if file.next_is('=')? {
+                    Self::MinusEq
+                } else {
+                    Self::Minus
+                }
+            }
+            '*' => {
+                if file.next_is('=')? { Self::StarEq } else { Self::Star }
+            }
+            '/' => {
+                if file.next_is('=')? { Self::SlashEq } else { Self::Slash }
+            }
+            '%' => {
+                if file.next_is('=')? { Self::PercentEq } else { Self::Percent }
+            }
+            '^' => {
+                if file.next_is('=')? { Self::CaretEq } else { Self::Caret }
+            }
+            '&' => {
+                if file.next_is('=')? {
+                    Self::AmpersandEq
+                } else if file.next_is('&')? {
+                    Self::DoubleAmpersand
+                } else {
+                    Self::Ampersand
+                }
+            }
+            '|' => {
+                if file.next_is('=')? {
+                    Self::PipeEq
+                } else if file.next_is('|')? {
+                    Self::DoublePipe
+                } else {
+                    Self::Pipe
+                }
+            }
+            '~' => Self::Tilde,
+            '?' => Self::Question,
+            '!' => {
+                if file.next_is('=')? { Self::NotEq } else { Self::Exclamation }
+            }
+            '.' => Self::Dot,
+            ',' => Self::Comma,
+            ':' => Self::Colon,
+            ';' => Self::SemiColon,
+            '=' => {
+                if file.next_is('=')? { Self::DoubleEq } else { Self::Eq }
+            }
+            '<' => {
+                if file.next_is('=')? {
+                    Self::LtEq
+                } else if file.next_is('<')? {
+                    if file.next_is('=')? { Self::LeftShiftEq } else { Self::LeftShift }
+                } else {
+                    Self::Lt
+                }
+            }
+            '>' => {
+                if file.next_is('=')? {
+                    Self::GtEq
+                } else if file.next_is('>')? {
+                    if file.next_is('=')? { Self::RightShiftEq } else { Self::RightShift }
+                } else {
+                    Self::Gt
+                }
+            }
+            '{' => Self::LBrace,
+            '}' => Self::RBrace,
+            '[' => Self::LBracket,
+            ']' => Self::RBracket,
+            '(' => Self::LParen,
+            ')' => Self::RParen,
+            '#' => Self::Hash,
+            '$' => Self::Dollar,
+            '@' => Self::At,
+            '\\' => Self::Backslash,
+            '\n' => Self::Newline,
+            _ => panic!("Invalid Punctuation"), //Check the is_punct_char function before calling this
+        })
+    }
 }
 
 pub enum TokenType {
@@ -93,192 +275,25 @@ pub struct TokenStream {
 }
 
 impl TokenStream {
-    pub fn new(mut file: SourceFile) -> Result<Self, TokenError> {
+    pub fn new(mut file: SourceFile) -> Result<Self> {
         let mut tokens = VecDeque::new();
 
         loop {
             let (c, pos) = match file.peek() {
                 Ok((c, pos)) => (c, pos),
-                Err(SourceFileError::EOF) => break,
+                Err(SourceFileError::EOF) => {
+                    break;
+                }
                 Err(SourceFileError::IoError(e)) => panic!("Error: {:?}", e.to_string()),
             };
 
             let token_type = match c {
-                '0'..='9' => {
-                    let (mut number, _) = file.until(|c| !matches!(c, '0'..='9'))?;
-
-                    if let Result::Ok(Some((c, _))) = file.next_if(|c| c == '.') {
-                        number.push(c);
-                        number.push_str(&file.until(|c| !matches!(c, '0'..='9'))?.0);
-                    }
-
-                    TokenType::Literal(Literal::Number(number))
-                }
+                '0'..='9' => TokenType::Literal(Literal::parse_number(&mut file)?),
                 quote @ ('"' | '\'') => {
-                    let (mut string, _) = file.until(|c| c == quote || c == '\\')?;
-
-                    match file.peek()?.0 {
-                        append @ ('\\' | '"' | '\'') => {
-                            file.next()?;
-                            string.push(append)
-                        }
-                        'n' => {
-                            file.next()?;
-                            string.push('\n')
-                        }
-                        'r' => {
-                            file.next()?;
-                            string.push('\r')
-                        }
-                        't' => {
-                            file.next()?;
-                            string.push('\t')
-                        }
-                        '0' => {
-                            file.next()?;
-                            string.push('\0')
-                        }
-                        'x' => {
-                            file.next()?;
-                            let (hex, _) = file.next_seq(2)?;
-                            let char_code = match u8::from_str_radix(&hex, 16) {
-                                Ok(code) => code,
-                                Err(_) => return Err(TokenError::InvalidEscape(hex)),
-                            };
-                            string.push(char_code as char);
-                        }
-                        other @ _ => {
-                            return Err(TokenError::InvalidEscape(other.to_string()));
-                        }
-                    }
-
-                    TokenType::Literal(Literal::String(string))
+                    TokenType::Literal(Literal::parse_string(&mut file, quote)?)
                 }
 
-                '+' => {
-                    if file.next_is('=')? {
-                        TokenType::Punct(Punct::PlusEq)
-                    } else {
-                        TokenType::Punct(Punct::Plus)
-                    }
-                }
-                '-' => {
-                    if file.next_is('>')? {
-                        if file.next_is('>')? {
-                            TokenType::Punct(Punct::FatArrow)
-                        } else {
-                            TokenType::Punct(Punct::Arrow)
-                        }
-                    } else if file.next_is('=')? {
-                        TokenType::Punct(Punct::MinusEq)
-                    } else {
-                        TokenType::Punct(Punct::Minus)
-                    }
-                }
-                '*' => {
-                    if file.next_is('=')? {
-                        TokenType::Punct(Punct::StarEq)
-                    } else {
-                        TokenType::Punct(Punct::Star)
-                    }
-                }
-                '/' => {
-                    if file.next_is('=')? {
-                        TokenType::Punct(Punct::SlashEq)
-                    } else {
-                        TokenType::Punct(Punct::Slash)
-                    }
-                }
-                '%' => {
-                    if file.next_is('=')? {
-                        TokenType::Punct(Punct::PercentEq)
-                    } else {
-                        TokenType::Punct(Punct::Percent)
-                    }
-                }
-                '^' => {
-                    if file.next_is('=')? {
-                        TokenType::Punct(Punct::CaretEq)
-                    } else {
-                        TokenType::Punct(Punct::Caret)
-                    }
-                }
-                '&' => {
-                    if file.next_is('=')? {
-                        TokenType::Punct(Punct::AmpersandEq)
-                    } else if file.next_is('&')? {
-                        TokenType::Punct(Punct::DoubleAmpersand)
-                    } else {
-                        TokenType::Punct(Punct::Ampersand)
-                    }
-                }
-                '|' => {
-                    if file.next_is('=')? {
-                        TokenType::Punct(Punct::PipeEq)
-                    } else if file.next_is('|')? {
-                        TokenType::Punct(Punct::DoublePipe)
-                    } else {
-                        TokenType::Punct(Punct::Pipe)
-                    }
-                }
-                '~' => TokenType::Punct(Punct::Tilde),
-                '?' => TokenType::Punct(Punct::Question),
-                '!' => {
-                    if file.next_is('=')? {
-                        TokenType::Punct(Punct::NotEq)
-                    } else {
-                        TokenType::Punct(Punct::Exclamation)
-                    }
-                }
-                '.' => TokenType::Punct(Punct::Dot),
-                ',' => TokenType::Punct(Punct::Comma),
-                ':' => TokenType::Punct(Punct::Colon),
-                ';' => TokenType::Punct(Punct::SemiColon),
-                '=' => {
-                    if file.next_is('=')? {
-                        TokenType::Punct(Punct::DoubleEq)
-                    } else {
-                        TokenType::Punct(Punct::Eq)
-                    }
-                }
-                '<' => {
-                    if file.next_is('=')? {
-                        TokenType::Punct(Punct::LtEq)
-                    } else if file.next_is('<')? {
-                        if file.next_is('=')? {
-                            TokenType::Punct(Punct::LeftShiftEq)
-                        } else {
-                            TokenType::Punct(Punct::LeftShift)
-                        }
-                    } else {
-                        TokenType::Punct(Punct::Lt)
-                    }
-                }
-                '>' => {
-                    if file.next_is('=')? {
-                        TokenType::Punct(Punct::GtEq)
-                    } else if file.next_is('>')? {
-                        if file.next_is('=')? {
-                            TokenType::Punct(Punct::RightShiftEq)
-                        } else {
-                            TokenType::Punct(Punct::RightShift)
-                        }
-                    } else {
-                        TokenType::Punct(Punct::Gt)
-                    }
-                }
-                '{' => TokenType::Punct(Punct::LBrace),
-                '}' => TokenType::Punct(Punct::RBrace),
-                '[' => TokenType::Punct(Punct::LBracket),
-                ']' => TokenType::Punct(Punct::RBracket),
-                '(' => TokenType::Punct(Punct::LParen),
-                ')' => TokenType::Punct(Punct::RParen),
-                '#' => TokenType::Punct(Punct::Hash),
-                '$' => TokenType::Punct(Punct::Dollar),
-                '@' => TokenType::Punct(Punct::At),
-                '\\' => TokenType::Punct(Punct::Backslash),
-
-                '\n' => TokenType::Punct(Punct::Newline),
+                punct if Punct::is_punct_char(punct) => TokenType::Punct(Punct::parse(&mut file)?),
 
                 mut space @ (' ' | '\t') => {
                     let mut len = 0;
@@ -295,13 +310,16 @@ impl TokenStream {
                 }
 
                 '_' | 'a'..='z' | 'A'..='Z' => {
-                    let (ident, _) =
-                        file.until(|c| !matches!(c, 'a'..='z' | 'A'..='Z' | '0'..='9' | '_'))?;
+                    let (ident, _) = file.until(
+                        |c| !matches!(c, 'a'..='z' | 'A'..='Z' | '0'..='9' | '_')
+                    )?;
 
                     TokenType::Ident(Ident(ident))
                 }
 
-                _ => return Err(TokenError::UnexpectedChar(c)),
+                _ => {
+                    return Err(TokenError::UnexpectedChar(c));
+                }
             };
 
             tokens.push_back((token_type, pos));
